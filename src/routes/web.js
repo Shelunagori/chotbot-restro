5ff35940fd60aab4567aaa7dfc902d0700161d15
Shelunagori <shelunagori@gmail.com>
2021-08-01 15:11:01 +0530

    update router

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,11 @@
 import express from "express";
 import homepageController from "../controllers/homepageController";
-import facebookController, { getWebhook } from "../controllers/facebookController";
+import facebookController from "../controllers/facebookController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/",homepageController.getHomepage);
-    router.get("/webhook",facebookController,getWebhook);
+    router.get("/webhook",facebookController.getWebhook);
     router.post("/webhook",facebookController.postWebhook);
     return app.use("/",router);
 }
