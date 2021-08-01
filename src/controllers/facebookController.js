e72ef8450003b84a042e6784e16da78a20a3ef62
Shelunagori <shelunagori@gmail.com>
2021-08-01 19:53:24 +0530

    handle message

--- src/controllers/facebookController.js
+++ src/controllers/facebookController.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 import request from  "request";
 const FACEBOOK_TOKEN = process.env.FACEBOOK_TOKEN;
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const PAGE_ACCESS_TOKEN = process.env.FB_PAGE_TOKEN;
 let postWebhook = (req,res) => {
     // Parse the request body from the POST
     let body = req.body;
